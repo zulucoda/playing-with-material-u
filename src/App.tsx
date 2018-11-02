9a6ac28a0c59d52e9f7f0106e0f2325ba92dedc4
Muzi (zulucoda) Buthelezi <muzi@mycrm.com>
2018-11-02 11:49:15 +0400

    [muzi] playing material ui :smile:

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,10 @@
 import * as React from 'react';
 import './App.css';
-
+import { AllButtons } from './components/Buttons/AllButtons';
+import { TestButton } from './components/Buttons/Button';
 import logo from './logo.svg';
+import { SampleFormView } from './modules/SampleForm/components';
+
 
 class App extends React.Component {
   public render() {
@@ -13,6 +16,18 @@ class App extends React.Component {
         </header>
         <p className="App-intro">
           To get started, edit <code>src/App.tsx</code> and save to reload.
+        </p>
+        <p>
+          <h3>One Button</h3>
+          <TestButton />
+        </p>
+        <p>
+          <h3>All Buttons</h3>
+          <AllButtons />
+        </p>
+        <p>
+          <h3>Simple Form using Grid Layout</h3>
+          <SampleFormView />
         </p>
       </div>
     );
